0577063e64b3f1d0c01e0ee31b0bd4a2f176e335
FFFiend <owais.zahid@mail.utoronto.ca>
2024-01-02 04:28:02 -0500

    finished landing page code

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Fullstack + ML app for learning ASL
 
 
 Node.js, S3 bucket, React (TailwindCSS), HuggingFace library
+
+# Current TODOS
+Design the following:
+Home Page
+[land_page.png](Landing Page)
--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,33 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hey there</h1>
+      <h1 className="text-8xl font-bold text-center">
+        Welcome to Sign It!
+      </h1>
+      <p className="text-5xl font-bold text-center">
+        Learn ASL today through our interactive platform.
+      </p>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <p className="text-center"> 
+      <button className="align-middle rounded-full bg-green-800 px-10 text-4xl">
+        Login to start playing.
+      </button>
+      </p>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <p className="text-center"> 
+      <button className="rounded-full bg-green-800 px-10 text-4xl">
+        Or sign up! Don't be shy.
+      </button>
+      </p>
     </>
   )
 }
